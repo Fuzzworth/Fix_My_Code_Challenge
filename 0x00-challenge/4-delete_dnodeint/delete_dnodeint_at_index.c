617b525678a87706fa39d98b8308c7fa801d196e
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-08-28 00:24:57 +0200

    fiX

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,10 +43,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->prev = (*head)->prev;
+		(*head)->prev->next = (*head)->next;
+		(*head)->next->prev = (*head)->prev;
 		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
 		*head = saved_head;
 	}
 	return (1);
