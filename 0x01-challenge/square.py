29147fc0ddb0f037ccbfdc12e74fb9ce3f02bf2a
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-10-18 04:23:12 +0200

    task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python3
-"""
-DOCS
-"""
+""" Module Docs"""
 
 
-class Square:
-    """
-    DOCS
-    """
+class Square():
+    """ Class Docs"""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """ DOCS """
+        """ Function Docs """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+        """ Area Docs"""
+        return self.width * self.height
 
     def permiter_of_my_square(self):
-        """ DOCS """
+        """ Perimeter Docs """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ DOCS """
+        """ STR DOCS """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """ DOCS """
+    """ Final DOcs """
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
